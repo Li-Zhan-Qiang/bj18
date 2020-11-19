b76c4da567f4e59826a2bebd310f651c8f19ab98
Li <434527054@qq.com>
2020-11-18 21:08:03 -0800

    添加创建视图login

--- views.py
+++ views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse('hello')
+
+
+def login(request):
+    return redirect('/index')
